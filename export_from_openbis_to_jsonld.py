f7aa60bf215d0f6c58f1437ecc47ddf859fa0556
Da Costa Lopes <fabio.dacostalopes@empa.ch>
2024-02-08 10:44:59 +0100

    Update metadata file and export_from_openbis_to_jsonld.py

--- export_from_openbis_to_jsonld.py
+++ export_from_openbis_to_jsonld.py
@@ -12,6 +12,10 @@ import json
 import copy
 
 #%% Functions
+
+def remove_digits_from_string(string):
+    return ''.join([i for i in string if not i.isdigit()])
+
 def is_nan(var):
     """Function to verify whether it is a NaN."""
     return var != var
@@ -26,6 +30,24 @@ def log_in(bisurl='openbis', bisuser='admin', bispasswd='changeit'):
     return session
 
 def get_parent_child_relationships_nested(selected_object, parent_child_relationships=None):
+    """
+    Function to get all the parent-child relations together with the information about the objects from openBIS.
+    This is a recursive function because the objects inside openBIS are like trees containing multiple
+    relations with other objects.
+
+    Parameters
+    ----------
+    selected_object : pybis.sample.Sample
+        Selected openBIS object.
+    parent_child_relationships : dict, optional
+        Dictionary containing the openBIS objects and the relations between them. The default is None.
+
+    Returns
+    -------
+    parent_child_relationships : TYPE
+        Dictionary containing the openBIS objects and the relations between them.
+
+    """
     if parent_child_relationships is None:
         parent_child_relationships = {}
 
@@ -44,21 +66,95 @@ def get_parent_child_relationships_nested(selected_object, parent_child_relation
 
     return parent_child_relationships
 
-def convert_openbis_ontology(key, schema, openbis_ontology_map, mapped_schema = None):
+def convert_openbis_ontology(schema, openbis_ontology_map, mapped_schema = None):
+    """
+    Function to convert the openBIS names into names mapped in the context part of the JSON-LD file.
+    It is a step necessary before creating the JSON-LD file.
+    This function is recursive as there are many nodes inside the dictionary.
+
+    Parameters
+    ----------
+    schema : dict
+        Dictionary with all the information of the object that one wants to export.
+    openbis_ontology_map : dict
+        Dictionary to map openBIS property names into ontology names.
+    mapped_schema : dict, optional
+        Updated schema. The default is None.
+
+    Returns
+    -------
+    mapped_schema : dict
+        Updated schema.
+
+    """
     if mapped_schema == None:
         mapped_schema = copy.deepcopy(schema)
-        
+    
     for key in schema:
+        # Convert dictionary keys from openBIS names by ontology names
         if key in openbis_ontology_map:
             new_key = openbis_ontology_map[key]
             mapped_schema[new_key] = mapped_schema.pop(key)
         else:
             new_key = key
+        
+        # Convert properties with hasProperty in ontology column by a dictionary
+        if "hasProperty" in new_key:
+            new_key_split = new_key.split("-hasProperty-")
+            first_property = new_key_split[0]
+            second_property = new_key_split[1]
+            mapped_schema[first_property] = {second_property: mapped_schema[new_key]}
+            mapped_schema.pop(new_key)
+        
+        # Replace object codes by object relations using hasPart
+        if "/" in new_key:
+            new_key_split = new_key.split("/")
+            object_code = new_key_split[-1]
+            object_code = remove_digits_from_string(object_code) # Get openBIS object code
+            
+            old_key = new_key
+            mapped_schema[old_key]["@type"] = openbis_ontology_map[object_code] # Add @type to the object
+            
+            new_key = f"hasPart-{old_key}"
+            mapped_schema[new_key] = mapped_schema.pop(old_key) # Replace openBIS object path by hasPart relation
             
         if type(schema[key])==dict:
-            convert_openbis_ontology(key, schema[key], openbis_ontology_map, mapped_schema[new_key])
+            convert_openbis_ontology(schema[key], openbis_ontology_map, mapped_schema[new_key])
     
     return mapped_schema
+
+def merge_hasPart_dicts(schema):
+    """
+    Function to merge all the dictionaries related to hasPart keys into a list of dictionaries.
+
+    Parameters
+    ----------
+    dictionary : dict
+        Object schema.
+
+    Returns
+    -------
+    merged_dicts : dict
+        Schema with all hasPart properties merged into list of dictionaries.
+
+    """
+    merged_dicts = {}
+
+    for key, value in schema.items():
+        if isinstance(value, dict):
+            # Recursively merge dictionaries in sublevels
+            merged_value = merge_hasPart_dicts(value)
+            if key.startswith("hasPart"):
+                # Merge dictionaries with keys starting with "hasPart" into a list
+                if "hasPart" not in merged_dicts:
+                    merged_dicts["hasPart"] = []
+                merged_dicts["hasPart"].append(merged_value)
+            else:
+                merged_dicts[key] = merged_value
+        else:
+            merged_dicts[key] = value
+
+    return merged_dicts
 
 #%% Import metadata schema
 metadata_schema_filename = "Metadata Schema_version3.xlsx"
@@ -69,7 +165,7 @@ objects_ontology = pd.read_excel(metadata_schema_filename, sheet_name = "Ontolog
 openbis_ontology_map = {}
 for _, row in objects_metadata.iterrows():
     openbis_parameter = row["openBIS"]
-    if is_nan(openbis_parameter) == False and is_nan(row["Unit"]) == False:
+    if is_nan(openbis_parameter) == False and is_nan(row["Ontology"]) == False:
         openbis_ontology_map[openbis_parameter] = row["Ontology"]
 
 #%% Prepare @context for JSON-LD
@@ -83,7 +179,7 @@ for _, row in objects_ontology.iterrows():
         entity_type = f"xsd:{entity_type}"
     
     if is_nan(row["IRI"]):
-        jsonld_schema["@context"][row["Entity"]] = {"@type": entity_type}
+        pass
     else:
         jsonld_schema["@context"][row["Entity"]] = {"@id": row["IRI"], "@type": entity_type}
 
@@ -100,12 +196,13 @@ parent_child_relationships = {openbis_selected_object_id: openbis_selected_objec
 selected_object_schema = get_parent_child_relationships_nested(openbis_selected_object, parent_child_relationships)
 
 #%% Prepare data for JSON-LD
-selected_object_schema_for_jsonld = convert_openbis_ontology(openbis_selected_object_id, selected_object_schema, openbis_ontology_map)
+selected_object_schema_converted = convert_openbis_ontology(selected_object_schema[openbis_selected_object_id], openbis_ontology_map)
+selected_object_schema_for_jsonld = merge_hasPart_dicts(selected_object_schema_converted)
 jsonld_schema["@graph"] = [selected_object_schema_for_jsonld]
 
 #%% Export to JSON-LD
 jsonld_object = json.dumps(jsonld_schema, indent=4)
  
 # Writing to sample.json
-with open("publication_schema.jsonld", "w") as outfile:
+with open("selected_object_schema.jsonld", "w") as outfile:
     outfile.write(jsonld_object)
